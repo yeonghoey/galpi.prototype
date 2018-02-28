37066be88363fe0355202b0c42065a1eb3fb6c2e
Yeongho Kim <yeonghoey@gmail.com>
2018-02-28 21:39:46 +0900

    Update dependencies

--- galpi/__init__.py
+++ galpi/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 
 
+__version__ = '0.0.1'
+
+
 app = Flask(__name__)
 
 
--- setup.py
+++ setup.py
@@ -1,8 +1,40 @@
-from setuptools import setup
+import ast
+import re
+from setuptools import find_packages, setup
+
+
+def extract_version(content):
+    m = re.search(r'__version__\s+=\s+(.*)', content)
+    s = m.group(1)
+    return str(ast.literal_eval(s))
+
+
+with open('galpi/__init__.py', 'rb') as f:
+    content = f.read().decode('utf-8')
+    version = extract_version(content)
+
 
 setup(
     name='galpi',
-    packages=['galpi'],
+    version=version,
+
+    description='Social Bookmarks',
+    url='https://github.com/yeonghoey/galpi',
+
+    author='Yeongho Kim',
+    author_email='yeonghoey@gmail.com',
+
+    packages=find_packages(),
     include_package_data=True,
-    install_requires=['flask'],
+    install_requires=[
+        'flask',
+        'zappa',
+    ],
+
+    license='MIT',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
