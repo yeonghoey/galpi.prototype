8610feb094792aac02ad9cc69f5801d798b0ed10
Yeongho Kim <yeonghoey@gmail.com>
2018-03-04 22:44:34 +0900

    Working on frontend

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import '../css/style.css';
-
-setTimeout(() => alert('Hi'), 1000);
+import './css/style.css';
--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,19 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
+
   },
   output: {
     path: path.join(__dirname, 'galpi/static'),
